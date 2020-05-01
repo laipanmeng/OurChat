bf258bcbf1d6ef6f3764b6c70d5bf002415c8a41
Linsane01 <linsane_4110@163.com>
2020-05-01 23:42:06 +0800

    1.test

--- app/src/main/java/com/lpm_wxl/ourchat/MainActivity.java
+++ app/src/main/java/com/lpm_wxl/ourchat/MainActivity.java
@@ -3,6 +3,7 @@ package com.lpm_wxl.ourchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("103020", "onCreate: ");
     }
 }
